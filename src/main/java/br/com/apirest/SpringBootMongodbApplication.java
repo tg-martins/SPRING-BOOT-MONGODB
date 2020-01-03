115992c7c0a81cd364e6194355293e2a2e7366c6
Thiago Martins <contato.thiagocesar@icloud.com>
2020-01-03 19:50:24 -0300

    Primeiro commit

---
+++ src/main/java/br/com/apirest/SpringBootMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbApplication.class, args);
+	}
+
+}
