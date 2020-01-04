497e019ae3516e1b920373272052c04a842eabb4
Thiago Martins <contato.thiagocesar@icloud.com>
2020-01-04 16:25:59 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/apirest/services/PostService.java
@@ -0,0 +1,23 @@
+package br.com.apirest.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.apirest.entities.Post;
+import br.com.apirest.repository.PostRepository;
+import br.com.apirest.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
