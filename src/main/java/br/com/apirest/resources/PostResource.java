649055703172ad3b4c39ca80a5cf6e69a25245c7
Thiago Martins <contato.thiagocesar@icloud.com>
2020-01-04 17:42:48 -0300

    Consulta simples com query methods

--- src/main/java/br/com/apirest/resources/PostResource.java
+++ src/main/java/br/com/apirest/resources/PostResource.java
@@ -1,13 +1,17 @@
 package br.com.apirest.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.apirest.entities.Post;
+import br.com.apirest.resources.util.URL;
 import br.com.apirest.services.PostService;
 
 @RestController
@@ -21,6 +25,14 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> posts = service.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
+
 	}
 
 }
