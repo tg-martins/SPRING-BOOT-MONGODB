e9eaaf4d1dbd0f5e957000c0dfc3f9628a713dd9
Thiago Martins <contato.thiagocesar@icloud.com>
2020-01-03 22:33:51 -0300

    Insercao de usuario com POST

--- src/main/java/br/com/apirest/resources/UserResource.java
+++ src/main/java/br/com/apirest/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.apirest.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -8,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.apirest.dto.UserDTO;
 import br.com.apirest.entities.User;
@@ -38,6 +42,15 @@ public class UserResource {
 			return ResponseEntity.ok().body(new UserDTO(obj.get()));
 		}
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody UserDTO dto, UriComponentsBuilder uriBuilder){
+		User user = service.insert(service.fromDTO(dto));
+		
+		URI uri = uriBuilder.path("/users/{id}").buildAndExpand(user.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(user);
 	}
 
 }
--- src/main/java/br/com/apirest/services/UserService.java
+++ src/main/java/br/com/apirest/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.apirest.dto.UserDTO;
 import br.com.apirest.entities.User;
 import br.com.apirest.repository.UserRepository;
 
@@ -21,10 +22,16 @@ public class UserService {
 
 	public Optional<User> findById(String id) {
 		Optional<User> obj = repository.findById(id);
-		
+
 		return obj;
-		
-		 
+	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO dto) {
+		return new User(dto.getId(), dto.getName(), dto.getEmail());
 	}
 
 }
