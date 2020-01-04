4198a34fec5330f8d56944b7168c27abf1ec165e
Thiago Martins <contato.thiagocesar@icloud.com>
2020-01-03 22:10:25 -0300

    Obtendo um usuario por id

--- src/main/java/br/com/apirest/resources/UserResource.java
+++ src/main/java/br/com/apirest/resources/UserResource.java
@@ -1,12 +1,14 @@
 package br.com.apirest.resources;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.apirest.dto.UserDTO;
@@ -20,10 +22,22 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+
+		Optional<User> obj = service.findById(id);
+
+		if (obj.isPresent()) {
+			return ResponseEntity.ok().body(new UserDTO(obj.get()));
+		}
+		return ResponseEntity.notFound().build();
+	}
+
 }
--- src/main/java/br/com/apirest/services/UserService.java
+++ src/main/java/br/com/apirest/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.apirest.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,14 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public Optional<User> findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		
+		return obj;
+		
+		 
 	}
 
 }
